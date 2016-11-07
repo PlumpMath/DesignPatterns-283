5f989271e13c700609c9a0ed56512128ee24261f
Gerardo <gerardomurias@hotmail.com>
2016-11-07 19:49:39 +0100

    added composite pattern

---
+++ DesignPatternsSolution/CompositePattern/Component.cs
@@ -0,0 +1,17 @@
+﻿namespace CompositePattern
+{
+    abstract class Component
+    {
+        protected string name;
+
+        // Constructor
+        public Component(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Add(Component c);
+        public abstract void Remove(Component c);
+        public abstract void Display(int depth);
+    }
+}
